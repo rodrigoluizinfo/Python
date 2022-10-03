eccbdf3efc41461031fb88d728d18e962cd73ff9
Rodrigo <rodrigoluizinfo@gmail.com>
2022-10-03 17:52:00 -0300

    Update repository

--- Exercicios/Análise de dados/Cusro_Python_Pandas_Digital_Innovation-master/introducao_biblioteca_pandas.py
+++ Exercicios/Análise de dados/Cusro_Python_Pandas_Digital_Innovation-master/introducao_biblioteca_pandas.py
@@ -1,19 +1,17 @@
-#Step one - Importar a biblioteca pandas (Apelidar de pd)
+#Step ONE - Import library of pandas with alias (pd)
 import pandas as pd #Alias(Pseudonimo) = pd
 from openpyxl import Workbook #It's necessáry to run code
 
-#Step two - Declarar a variável file_name para receber o diretório(Copy path) do arquivo excel
+#Step TWO - Declarar a variável file_name para receber o diretório(Copy path) do arquivo excel
 #          Declarar variável df(Data Frame) para receber os dados variável file_name
-file_name = "C:/Users/rodri/OneDrive/Python/Exercicios/Análise de dados/Cusro_Python_Pandas_Digital_Innovation-master/datasets/Gapminder.csv"
+file_name1 = "C:/Users/rodri/OneDrive/Python/Exercicios/Análise de dados/Cusro_Python_Pandas_Digital_Innovation-master/datasets/Gapminder.csv"
 df = pd.read_csv(file_name, sep=';')#Sep - It will separeted the columns of the excel file with ;
 
-#Rename columns excel file
+#Rename columns excel file(Optional)
 df = df.rename(columns={"country":"País", "continent":"Continente", "year":"Ano", "lifeExp":"Expectativa de vida", "pop":"População total", "gdpPercap":"PIB"})
 
 # #Reading excel file
 # print(df.head())
-
-# #Reading excel file
 # print(df.head(10))#Show the ten firts lines excel file
 # print(df.shape)#Show total lines and columns
 # print(df.columns)
@@ -27,8 +25,10 @@ df = df.rename(columns={"country":"País", "continent":"Continente", "year":"Ano
 # print(object_oceania["Continente"].unique())#Show only one result of object "Continente"
 # print(df.groupby("Continente")["País"].nunique())#Shows count country each continent
 # print(df.groupby("Ano")["Expectativa de vida"].mean())#Shows lifes expectancy each year
-# print(df["PIB"].sum())
-# print(df["PIB"].mean())
+# print(df["PIB"].sum())#Sum of values of colum PIB
+# print(df["PIB"].mean())#Mean of values of colum PIB
+
+
 
 
 
---
+++ Exercicios/Análise de dados/Cusro_Python_Pandas_Digital_Innovation-master/trabalhando_com_planilhas_excel.py
@@ -0,0 +1,45 @@
+#Step ONE - Import library of pandas with alias (pd)
+import pandas as pd
+from openpyxl import Workbook #It's necessáry to run code
+
+#Step TWO - Declarar a variável file_name para receber o diretório(Copy path) do arquivo excel
+# Declarar variável df(Data Frame) para receber os dados variável file_name
+file_name1 = "C:/Users/rodri/OneDrive/Python/Exercicios/Análise de dados/Cusro_Python_Pandas_Digital_Innovation-master/datasets/Aracaju.xlsx"
+file_name2 = "C:/Users/rodri/OneDrive/Python/Exercicios/Análise de dados/Cusro_Python_Pandas_Digital_Innovation-master/datasets/Fortaleza.xlsx"
+file_name3 = "C:/Users/rodri/OneDrive/Python/Exercicios/Análise de dados/Cusro_Python_Pandas_Digital_Innovation-master/datasets/Fortaleza.xlsx"
+file_name4 = "C:/Users/rodri/OneDrive/Python/Exercicios/Análise de dados/Cusro_Python_Pandas_Digital_Innovation-master/datasets/Recife.xlsx"
+file_name5 = "C:/Users/rodri/OneDrive/Python/Exercicios/Análise de dados/Cusro_Python_Pandas_Digital_Innovation-master/datasets/Salvador.xlsx"
+
+df1 = pd.read_excel(file_name1, engine="openpyxl")
+df2 = pd.read_excel(file_name2, engine="openpyxl")
+df3 = pd.read_excel(file_name3, engine="openpyxl")
+df4 = pd.read_excel(file_name4, engine="openpyxl")
+df5 = pd.read_excel(file_name5, engine="openpyxl")
+
+df = pd.concat([df1, df2, df3, df4, df5])#Concatenate all data frames to one data frame(df)
+
+#print("")
+print('\n')
+print("Head = Cabeçalho - Show the first five lines")
+print(df.head())
+print('\n')
+print("Tail = rodapé - Show the first five lines")
+print(df.tail())
+print('\n')
+print("Show the five lines(Random)")
+print(df.sample())#Show the five lines(Random)
+print('\n')
+print("Show the types of values")
+print(df.dtypes)
+print('\n')
+
+print("Changing type LojaID to object")
+df["LojaID"] = df["LojaID"].astype("object")
+print(df.dtypes)
+
+#Class paused at 08:20
+
+
+
+
+
